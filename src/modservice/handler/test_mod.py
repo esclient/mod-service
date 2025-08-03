2209df94f10ef7759dfadf124cf0a742bac38c0e
Andrey Kataev <kata3v.andrey@yandex.ru>
2025-08-04 01:23:55 +0300

    Добавил линтеры (#5)

--- src/modservice/handler/test_mod.py
+++ src/modservice/handler/test_mod.py
@@ -1,7 +1,9 @@
-from modservice.grpc import mod_pb2
 import grpc
 
-def TestMod(request: mod_pb2.TestModRequest, context: grpc.ServicerContext):
-    return mod_pb2.TestModResponse(
-        success = True
-    )
+from modservice.grpc import mod_pb2
+
+
+def TestMod(
+    _: mod_pb2.TestModRequest, __: grpc.ServicerContext
+) -> mod_pb2.TestModResponse:
+    return mod_pb2.TestModResponse(success=True)
--- src/modservice/settings.py
+++ src/modservice/settings.py
@@ -1,16 +1,26 @@
+import logging
+
 from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    host: str = Field("0.0.0.0", env = "HOST")
-    port: int = Field(50051, env = "PORT")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
 
-    database_url: str = Field (..., env="DATABASE_URL")
+    host: str = Field(validation_alias="HOST")
+    port: int = Field(validation_alias="PORT")
+    database_url: str = Field(validation_alias="DATABASE_URL")
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    log_level: str = Field(validation_alias="LOG_LEVEL")
+    log_format: str = Field(validation_alias="LOG_FORMAT")
+    log_datefmt: str = Field(validation_alias="LOG_DATEFMT")
 
-
-settings = Settings()
+    def configure_logging(self) -> None:
+        logging.basicConfig(
+            level=self.log_level,
+            format=self.log_format,
+            datefmt=self.log_datefmt,
+        )
