ad18309dd23e547b9e05f5d504158661483b384f
timavar101 <gorodilovtima@mail.ru>
2025-08-23 18:24:16 +0600

    исправление багов в базе create mod и удаление test_mod

--- src/modservice/handler/create_mod.py
+++ src/modservice/handler/create_mod.py
@@ -8,9 +8,9 @@ def CreateMod(
     request: mod_pb2.CreateModRequest,
     _: grpc.ServicerContext,
 ) -> mod_pb2.CreateModResponse:
-    id = service.create_mod(
+    mod_id, upload_url, s3_key = service.create_mod(
         request.mod_title, request.author_id, request.filename, request.description
     )
     return mod_pb2.CreateModResponse(
-        mod_id=id, upload_url= , s3_key= 
+        mod_id=mod_id, upload_url=upload_url , s3_key=s3_key
     )
