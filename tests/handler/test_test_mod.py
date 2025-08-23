c0ac17599f8461e02d7d025598daa114bb222b09
timavar101 <gorodilovtima@mail.ru>
2025-08-23 18:43:56 +0600

    Update test_test_mod.py

--- tests/handler/test_test_mod.py
+++ tests/handler/test_test_mod.py
@@ -1,17 +1,17 @@
 import grpc
 from pytest_mock import MockerFixture
 
-from modservice.grpc.mod_pb2 import TestModRequest, TestModResponse
+from modservice.grpc.mod_pb2 import CreateModRequest, CreateModResponse
 from modservice.handler.test_mod import TestMod
 
 
 def test_testmod_returns_success_true(mocker: MockerFixture) -> None:
     ctx = mocker.Mock(spec=grpc.ServicerContext)
 
-    request = TestModRequest()
+    request = CreateModRequest()
     response = TestMod(request, ctx)
 
-    assert isinstance(response, TestModResponse)
+    assert isinstance(response, CreateModResponse)
     assert response.success is True
 
-    assert response == TestModResponse(success=True)
+    assert response == CreateModResponse(success=True)
