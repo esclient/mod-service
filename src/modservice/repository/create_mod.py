c2ac17efa3544f976ec33481634ca31fa6bacabc
Tecquo <46904988+Tecquo@users.noreply.github.com>
2025-08-26 13:50:50 +0300

    Вынес генерацию url в сервисный слой,

--- src/modservice/repository/create_mod.py
+++ src/modservice/repository/create_mod.py
@@ -3,14 +3,27 @@ from psycopg2.pool import ThreadedConnectionPool
 
 def create_mod(
     db_pool: ThreadedConnectionPool,
-    mod_title: str,  # noqa: ARG001
-    author_id: int,  # noqa: ARG001
-    filename: str,  # noqa: ARG001
-    description: str,  # noqa: ARG001
-) -> tuple[int, str, str]:
+    mod_title: str, # noqa: ARG001
+    author_id: int, # noqa: ARG001
+    filename: str, # noqa: ARG001
+    description: str, # noqa: ARG001
+    s3_key: str, # noqa: ARG001
+) -> int:
     conn = db_pool.getconn()
     try:
-        with conn.cursor():
+        with conn.cursor() as cursor:
+            # Вставка нового мода в базу данных - на будущее для работы с бд
+            # cursor.execute(
+            #     """
+            #     INSERT INTO mods (mod_title, author_id, filename, description, s3_key)
+            #     VALUES (%s, %s, %s, %s, %s)
+            #     RETURNING id
+            #     """,
+            #     (mod_title, author_id, filename, description, s3_key)
+            # )
+            # result = cursor.fetchone()
+            # conn.commit()
+            # return result[0] if result else 0
             return 1, "upload_url", "s3_key"
     finally:
         db_pool.putconn(conn)
