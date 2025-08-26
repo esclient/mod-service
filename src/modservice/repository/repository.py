89cca268060d2df0cfa456254830e00f63ff77f5
Tecquo <46904988+Tecquo@users.noreply.github.com>
2025-08-26 15:06:57 +0300

    Reapply "Вынес генерацию url в сервисный слой,"
    This reverts commit b5994ea7c8598615e2fc96a313410d3f990586c0.

--- src/modservice/repository/repository.py
+++ src/modservice/repository/repository.py
@@ -8,8 +8,8 @@ class ModRepository:
         self._db_pool = db_pool
 
     def create_mod(
-        self, mod_title: str, author_id: int, filename: str, description: str
-    ) -> tuple[int, str, str]:
+        self, mod_title: str, author_id: int, filename: str, description: str, s3_key: str
+    ) -> int:
         return _create_mod(
-            self._db_pool, mod_title, author_id, filename, description
+            self._db_pool, mod_title, author_id, filename, description, s3_key
         )
